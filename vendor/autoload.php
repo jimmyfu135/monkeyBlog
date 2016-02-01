e9cf6879adffc86fa6f22a4555ca9e92f6baf475
jimmyfu135 <jimmyfu135@hotmail.com>
2016-02-01 23:08:36 +0800

    update vendor info

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2b7d03517eb99c3de3e326358fc9577f::getLoader();
+return ComposerAutoloaderInit57490ad1dcfe79a8596ecec07c4ed5e2::getLoader();
